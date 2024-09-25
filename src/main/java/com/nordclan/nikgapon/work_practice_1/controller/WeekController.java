09e128353d04ac0495506eccd684580c1d2cb78d
NikGapon <nik.gapon84@yandex.ru>
2024-09-25 15:21:19 +0400

    Week calendar without html + many small fix

--- src/main/java/com/nordclan/nikgapon/work_practice_1/controller/WeekController.java
+++ src/main/java/com/nordclan/nikgapon/work_practice_1/controller/WeekController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Locale;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.*;
 
 @Controller
 @RequestMapping("/week")
@@ -26,16 +26,24 @@ public class WeekController {
             calendar.setTime(java.sql.Date.valueOf(LocalDate.now().plusDays(7 * weeknumber)));
         }
         calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+        Map<String, String> dateforCurentWeek = new HashMap<>();
+        //DateTimeFormatter datefomr = DateTimeFormatter.ofPattern("dd-MM-yyyy", Locale.ENGLISH);
+        SimpleDateFormat datefomr = new SimpleDateFormat("dd-MM-yyyy");
+
         for (int i = 0; i < 7; i++) {
             String nameOfMonth = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, locale);
             String nameOfDay = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, locale);
             int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
 
-            System.out.println(dayOfMonth + ": " + calendar.getTime());
+            //System.out.println(dayOfMonth + ": " + calendar.getTime());
             //23: Mon Sep 23 14:25:50 GMT+04:00 2024
             //24: Tue Sep 24 14:25:50 GMT+04:00 2024
             // 5. increase day field; add() will adjust the month if neccessary
+
+            System.out.println(nameOfDay +":" + datefomr.format(calendar.getTime()));
+            dateforCurentWeek.put("week", datefomr.format(calendar.getTime()));
             calendar.add(Calendar.DAY_OF_WEEK, 1);
+
         }
 
 
