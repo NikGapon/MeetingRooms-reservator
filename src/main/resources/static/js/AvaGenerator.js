30b6efcce2a269cf7e2538959d693a1fc45faae0
NikGapon <nik.gapon84@yandex.ru>
2024-10-10 16:03:35 +0400

    big upd for avatar generator

--- src/main/resources/static/js/AvaGenerator.js
+++ src/main/resources/static/js/AvaGenerator.js
@@ -17,6 +17,7 @@ function generateRGBFromDigit(digit) {
 function hexToDecimal(hex) {
     return BigInt(`0x${hex}`).toString();
 }
+
 (async function() {
 
     let canvas = document.createElement("canvas");
@@ -48,6 +49,12 @@ function hexToDecimal(hex) {
                 for (let j = 0; j < m[i].length; j++) {
                       m[i][j] = (i + j) % 2 == 0 ? 0 : 1;
                 }}
+        } else if(parseInt(n.toString()[0]) == 5){
+            for (let i = 0; i < m.length; i++) {
+                                        for (let j = 0; j < m[i].length; j++) {
+                                            m[i][j] = 1;
+                                        }
+                                    }
         } else{
          for (let i = 0; i < m.length; i++) {
                             for (let j = 0; j < m[i].length; j++) {
@@ -64,32 +71,66 @@ function hexToDecimal(hex) {
         b = rgb[2];
         ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 1)`;
 
-        if (parseInt(n.toString()[2]) >= 5){
+        if (parseInt(n.toString()[2]) > 5){
             ctx.beginPath();
             ctx.arc(50, 50, 25, 0, Math.PI * 2, true);
-
-
-            ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 1)`;
             ctx.fill();
-            ctx.stroke();
-        } else {
+        } else if(parseInt(n.toString()[2]) < 5) {
             ctx.beginPath();
             ctx.fillRect(25, 25, 50, 50);
-
             ctx.fill();
-            ctx.stroke();
+        }else{
+            ctx.beginPath();
+                        ctx.fillRect(0, 25, 100, 50);
+                        ctx.fill();
         }
-        drawMatrix(m, generateRGBFromDigit((n.toString())[1]));
+
          rgb = generateRGBFromDigit((n.toString())[4]);
          r = rgb[0];
          g = rgb[1];
          b = rgb[2];
+                 ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 1)`;
+
+            if (parseInt(n.toString()[5]) < 3) {
+                drawDiamond(40, 40, 8, 8);
+                drawDiamond(60, 40, 8, 8);
+            } else if (parseInt(n.toString()[5]) > 3 && parseInt(n.toString()[5]) < 6) {
+                drawTriangle(40, 40, 8);
+                drawTriangle(60, 40, 8);
+            } else if (parseInt(n.toString()[5]) > 6) {
+                drawEye(50, 40, 10);
+            } else {
+                drawEye(35, 40,4);
+                drawEye(65, 40, 4);
+            }
+
+
+            if (parseInt(n.toString()[6]) > 5) {
+                ctx.beginPath();
+                ctx.arc(50, 50, 15, 0.25 * Math.PI, 0.75 * Math.PI, false);
+                ctx.stroke();
+            } else {
+                    ctx.beginPath();
+                            ctx.moveTo(50 - 20 / 2, 65);
+                            ctx.lineTo(50 + 20 / 2, 65);
+                            ctx.stroke();
+            }
+
+            rgb = generateRGBFromDigit((n.toString())[7]);
+                     r = rgb[0];
+                     g = rgb[1];
+                     b = rgb[2];
+                             ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 1)`;
+            if (parseInt(n.toString()[8]) < 4) {
+                    drawCylinder(40, 10, 20, 15);
+             } else if (parseInt(n.toString()[8]) > 7) {
+                    drawConeHat(50, 10, 20, 30);
+                }
 
 
 
-            ctx.beginPath();
-            ctx.arc(50, 55, 20, 0, Math.PI, false);
-            ctx.stroke();
+
+
 
     }
 
@@ -97,6 +138,47 @@ function hexToDecimal(hex) {
         ctx.fillStyle = "#F8F8F8";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
+    function drawDiamond(x, y, w, h) {
+                ctx.beginPath();
+                ctx.moveTo(x, y - h / 2);
+                ctx.lineTo(x + w / 2, y);
+                ctx.lineTo(x, y + h / 2);
+                ctx.lineTo(x - w / 2, y);
+                ctx.closePath();
+                ctx.fill();
+            }
+            function drawTriangle(x, y, size) {
+                ctx.beginPath();
+                ctx.moveTo(x, y - size / 2);
+                ctx.lineTo(x + size / 2, y + size / 2);
+                ctx.lineTo(x - size / 2, y + size / 2);
+                ctx.closePath();
+                ctx.fill();
+            }
+            function drawEye(x, y, radius) {
+                        ctx.beginPath();
+                ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+                ctx.fill();
+            }
+            function drawCylinder(x, y, width, height) {
+                ctx.beginPath();
+                ctx.rect(x, y, width, height);
+                ctx.fill();
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.rect(x - 5, y + height, width + 10, 5);
+                ctx.fill();
+                ctx.stroke();
+            }
+               function drawConeHat(x, y, baseWidth, height) {
+                ctx.beginPath();
+                ctx.moveTo(x, y);
+                ctx.lineTo(x - baseWidth / 2, y + height);
+                ctx.lineTo(x + baseWidth / 2, y + height);
+                ctx.closePath();
+                ctx.fill();
+                ctx.stroke();
+            }
 
     function drawMatrix(m, rgb) {
         clear();
